7f4b7cfa9500802ae6a810af4dc4e5e8d05f6568
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-05 01:24:34 +0300

    created -> project

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import logo from './assets/pngegg.png'
+import './index.css'
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+<template lang="pug">
+
+
+header(class="bg-gray-800 text-white  flex justify-between items-center px-10")
+    .logo
+        img(:src="logo" alt="Vue logo" width="70" height="50")
+    nav(class="flex space-x-5") 
+        RouterLink(to="/") Home
+        RouterLink(to="/contact") Login
+main(class="text-red-400") Hakan
+RouterView
+footer This is the footer
+
+
+
+
+
+
+</template>
---
+++ frontend/src/views/PokemonDetail.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Pokemon Detail</h1>
+  </div>
+</template>
