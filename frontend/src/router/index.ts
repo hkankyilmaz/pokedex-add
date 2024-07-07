15bede89ada2e72048d7a985038e292d792d7d73
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-07 16:31:18 +0300

    edited routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PokemonDetail from '../views/PokemonDetail.vue'
+import PokemonList from '../components/PokemonList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/pokemon/:id', name: PokemonDetail, component: PokemonDetail }]
+  routes: [
+    { path: '/', name: 'Home', component: PokemonList },
+    { path: '/pokemon/:id', name: PokemonDetail, component: PokemonDetail }
+  ]
 })
 
 export default router
